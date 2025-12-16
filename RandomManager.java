896f6c749c2e3a19f870743e5b63eaf378ae7823
Francesco Pollarà <40788754+frax2004@users.noreply.github.com>
2025-12-16 18:20:44 +0100

    Update RandomManager.java

--- RandomManager.java
+++ RandomManager.java
@@ -1,34 +1,47 @@
+import java.util.stream.IntStream;
+
 public interface RandomManager {
   /// 13 => venerdi 13
   /// 67 => six-seven
   /// 89 => il capobranco dei suoi paguri
-  static final int[] PRIMES = new int[] { 13, 67, 89 };
-  static final int LEVEL_COUNT = 5;
-  static final int SCENARIO_COUNT = 4;
-  static final int EVENT_COUNT = 3;
+  static final int[] PRIMES = new int[] { 1, 13, 67, 89 };
+
+  static int convolution(int[] lhs, int[] rhs) {
+    assert lhs.length == rhs.length;
+    return IntStream
+    .range(0, lhs.length)
+    .map(i -> lhs[i]*rhs[i])
+    .sum();
+  }
 
   static int getMappedLevel(int seed, int levelIndex) {
-    return (seed + PRIMES[0]*levelIndex) % LEVEL_COUNT;
+    return convolution(PRIMES, new int[] {seed, levelIndex, 0, 0 }) % Levels.getPossibleLevels().length;
   }
 
   static int getMappedScenario(int seed, int levelIndex, int scenarioIndex) {
-    return (seed + PRIMES[0]*levelIndex + PRIMES[1]*scenarioIndex) % SCENARIO_COUNT;
+    return convolution(PRIMES, new int[] {seed, levelIndex, scenarioIndex, 0 }) % Scenarioes.getPossibleScenarios().length;
   }
 
   static int getMappedEvent(int seed, int levelIndex, int scenarioIndex, int eventIndex) {
-    return (seed + PRIMES[0]*levelIndex + PRIMES[1]*scenarioIndex + PRIMES[2]*eventIndex) % EVENT_COUNT;
+    return convolution(PRIMES, new int[] {seed, levelIndex, scenarioIndex, eventIndex }) % Events.getPossibleEvents().length;
   }
 
 }
 
 
-void use_case() {
-  Class<? extends Event>[] events = Events.getPossibileEvents();
 
-  /// seed 0, first level, first scenario, first possibile event index
+void use_case() {
+
+  List<Class<? extends Event>> events = Arrays.asList(Events.getPossibileEvents());
+  
+
+
   int[] indexes = new int[] {
+    /// seed 0, first level, first scenario, first possibile event index
     RandomManager.getMappedEvent(0, 0, 0, 0),
+    /// seed 0, first level, first scenario, second possibile event index
     RandomManager.getMappedEvent(0, 0, 0, 1),
+    /// seed 0, first level, first scenario, third possibile event index
     RandomManager.getMappedEvent(0, 0, 0, 2)
   };
 
